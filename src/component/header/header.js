8037ba9779374c640bb7de4f517df64abcf5761b
MrSr0ka <evv4ns@icloud.com>
2023-12-24 10:51:16 +0100

    second

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,11 +1,23 @@
 
 import React from 'react';
+import Logo from '../../images/logo.png'
 
 function Header() {
   return (
     <header>
     <div className='navi'>
+        <div className='navi__logoBox'>
+        <img className='logo' src={Logo} alt="Logo" />
         </div>
+        <div className='navi__linksBox'>
+        <ul className='navi__linksBox__linksList'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Clients</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+        </div>
+    </div>
     </header>
   );
 }
